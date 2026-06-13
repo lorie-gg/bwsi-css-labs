50acbd5300f33fcdaec75b43c901918d45955767
Alice R <alice.mei.rogers@gmail.com>
2026-06-12 17:20:55 -0700

    Add name to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -9,6 +9,7 @@ def main():
     print("Hello World!")
 
     name = "" # TODO: Insert your name between the double quotes
+    name = "Alice" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
 
